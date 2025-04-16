f13728a548e4ed7c506877cdfa00942429ccfe35
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-16 10:58:35 -0500

    added me resolver

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,10 +1,17 @@
-import Tech, { ITech } from '../models/Tech.js';
-import Matchup, { IMatchup } from '../models/Matchup.js';
+import User, { IUser } from '../models/User.js';
+import { AuthenticationError } from 'apollo-server-express';
+import { signToken } from '../utils/auth';
+import { BookInput } from '../types/book';
+import { GraphQLContext } from '../types/context';
+
 
 const resolvers = {
   Query: {
-    tech: async (): Promise<ITech[] | null> => {
-      return Tech.find({});
+    me: async (_parent: any, _args: any, context: GraphQLContext) => {
+      if (context.user) {
+        return User.findById(context.user._id);
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
     matchups: async (_parent: any, { _id }: { _id: string }): Promise<IMatchup[] | null> => {
       const params = _id ? { _id } : {};
