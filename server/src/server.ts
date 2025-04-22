115b6e691e484f1c12bd3f4b65b1d0215655481e
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-22 10:04:00 -0500

    removed path import from server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import path from 'path';
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
